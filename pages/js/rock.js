b4ce1ef2a5c852f3ff8ffcd035f0943614c6c964
maria magdalena <neumariamagdalena@gmail.com>
2024-02-12 12:57:33 +0100

    at least something but it's not workgin yet

--- pages/js/rock.js
+++ pages/js/rock.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+function getComputerChoice () {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return  choices[randomIndex];
+}
+
+console.log(getComputerChoice());
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    const winningCombos = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper'
+    };
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (winningCombos[playerSelection] === computerSelection) {
+        return "You won!";
+    } else { 
+        return "You lose!";
+    }
+}
+
+const playerSelection = 'Rock';
+const computerSelection = 'getComputerChoice';
+console.log(playRound(playerSelection, computerSelection));
