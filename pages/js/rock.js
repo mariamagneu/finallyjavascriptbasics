739480c92529c74354e10a6117720d8650014960
maria magdalena <neumariamagdalena@gmail.com>
2024-02-13 16:39:03 +0100

    pseudocode

--- pages/js/rock.js
+++ pages/js/rock.js
@@ -1,56 +1,10 @@
-console.log("Hello World");
+/* 
+get computer choice (random) - create a function
+get user choice (prompt) make smaller case
+write function that plays single round (parameters playerSelection & Computer Selection)
+return messages like you won you lost its a tie
+test function with console.log(playRound(playerSelection, computerSelection));
+write new function playGame and nest playRound inside of it. user OR statements or Loop. 
+console.log results of each round and the winner at the end.*/ 
 
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-}
 
-function playGame() {
-    function playRound(playerSelection) {
-        if (playerSelection === null) {
-            return "You canceled the game!";
-        }
-
-        playerSelection = playerSelection.toLowerCase();
-        const computerSelection = getComputerChoice();
-        const winningCombos = {
-            'rock': 'scissors',
-            'paper': 'rock',
-            'scissors': 'paper'
-        };
-
-        if (playerSelection === computerSelection.toLowerCase()) {
-            return "It's a tie!";
-        } else if (winningCombos[playerSelection] === computerSelection.toLowerCase()) {
-            return "You won!";
-        } else { 
-            return "You lose!";
-        }
-    }
-
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let round = 1; round <= 5; round++) {
-        const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
-        const result = playRound(playerSelection)
-        if (result === "You won!") {
-            playerScore++;
-        } else if (result === "You lose!") {
-            computerScore++;
-        }
-        console.log(`Round ${round}: ${result}`);
-    }
-
-    console.log("Game over!");
-
-    if (playerScore > computerScore) {
-        console.log("You won the game!!");
-    } else if (playerScore < computerScore) {
-        console.log("You lost the game :(")
-    } else {
-        console.log("It's a tie!");
-    }
-}
-
-playGame();
