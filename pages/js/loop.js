010e4d34c021e1a33445bcd0e223f9951bd864e4
maria magdalena <neumariamagdalena@gmail.com>
2024-02-20 12:41:38 +0100

    loop exercise with while loop & if statement & isPrime() function

--- pages/js/loop.js
+++ pages/js/loop.js
@@ -1,32 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-const name = 'Mustafa';
-const para = document.createElement('p');
+    
+    let i = 500;
+    const para = document.createElement('p');
 
-const phonebook = [
-  { name : 'Chris', number : '1549' },
-  { name : 'Li Kang', number : '9634' },
-  { name : 'Anne', number : '9065' },
-  { name : 'Francesca', number : '3001' },
-  { name : 'Mustafa', number : '6888' },
-  { name : 'Tina', number : '4312' },
-  { name : 'Bert', number : '7780' },
-  { name : 'Jada', number : '2282' },
-]
+    function isPrime(num) {
+      for(let i = 2; i < num; i++) {
+        if(num % i === 0) {
+          return false;
+        }
+      }
 
-// Add your code here
-let contactsHTML = '';
-for (let i = 0; i < phonebook.length; i++) {
-    const contact = phonebook[i];
-    if (contact.name === name) {
-        contactsHTML += `<p>${contact.name}: ${contact.number}</p>`;
-        para.innerHTML = contactsHTML;
+      return true;
     }
-}
-para.innerHTML = contactsHTML;
 
 
-// Don't edit the code below here!
-const section = document.querySelector('section');
-section.appendChild(para);
+    //  You need to use a loop to go through the numbers 2
+    // to 500 backwards (1 is not counted as a prime number), and run the
+    // provided isPrime() function on them. For each number that isn't a
+    // prime number, continue on to the next loop iteration.
+    // For each one that is a prime number, add it to the paragraph's 
+    //textContent along with some kind of separator.
+    
+    while (i >= 2) {
+        if (isPrime(i)) {
+            para.textContent += i + ', '; // Add prime numbers to the paragraph
+        }
+        i--;
+    }
+
+    // Don't edit the code below here!
+    const section = document.querySelector('section');
+    section.appendChild(para);
+
+
 });
