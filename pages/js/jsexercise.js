1812ed02722861afac84ace43c50d756483946ea
maria magdalena <neumariamagdalena@gmail.com>
2024-02-22 21:43:39 +0100

    create sumAll function that takes firstNumber and lastNumber as params. make it do a for loop where the start is i = firstNUmber, it iterates whilst i <=  lastNumber, and makes one step forward each time i++. in the loop define the variable sum and make it store the value of i and add on to itself on each iteration

--- pages/js/jsexercise.js
+++ pages/js/jsexercise.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
-};
+let sum = 0;
+const sumAll = function (firstNumber, lastNumber) { 
+    for (i = firstNumber; i <= lastNumber; i++) {
+      sum += i;
+        }
+        return sum;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
