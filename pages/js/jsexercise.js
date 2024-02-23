23d570013dc129784864b2407b1658d9b636b79b
maria magdalena <neumariamagdalena@gmail.com>
2024-02-23 20:47:38 +0100

    new exercise, test if year is a leap year

--- pages/js/jsexercise.js
+++ pages/js/jsexercise.js
@@ -1,23 +1,10 @@
-const sumAll = function(firstNumber, lastNumber) {
-    if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number') {
-        return "ERROR"; // Check if either argument is not a number
+const leapYears = function(number) {
+    if (number % 4 === 0 && number % 100 !== 0 || number % 400 === 0) {
+    return true;
+    } else {
+        return false;
     }
-    if (Array.isArray(firstNumber) || Array.isArray(lastNumber)) {
-        return "ERROR"; // Check if either argument is an array
-    }
-    if (firstNumber < 0 || lastNumber < 0) {
-        return "ERROR"; // Check if either number is negative
-    }
-
-    const minNumber = Math.min(firstNumber, lastNumber);
-    const maxNumber = Math.max(firstNumber, lastNumber);
-    let sum = 0;
-    for (let i = minNumber; i <= maxNumber; i++) {
-        if (Number.isInteger(i)) {
-            sum += i;
-        }
-    }
-    return sum;
 };
+
 // Do not edit below this line
 module.exports = sumAll;
