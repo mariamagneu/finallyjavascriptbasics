b2f59c1f7082ceefebe710a4c31379332dcb1776
maria magdalena <neumariamagdalena@gmail.com>
2024-02-23 20:33:35 +0100

    clean up code and put conditions outside of loop, add condition to test if param is an array

--- pages/js/jsexercise.js
+++ pages/js/jsexercise.js
@@ -1,18 +1,23 @@
-const sumAll = function (firstNumber, lastNumber) { 
-    let sum = 0;
+const sumAll = function(firstNumber, lastNumber) {
+    if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number') {
+        return "ERROR"; // Check if either argument is not a number
+    }
+    if (Array.isArray(firstNumber) || Array.isArray(lastNumber)) {
+        return "ERROR"; // Check if either argument is an array
+    }
+    if (firstNumber < 0 || lastNumber < 0) {
+        return "ERROR"; // Check if either number is negative
+    }
+
     const minNumber = Math.min(firstNumber, lastNumber);
     const maxNumber = Math.max(firstNumber, lastNumber);
-    for (i = minNumber; i <= maxNumber; i++) {
-        if (minNumber < 0 || maxNumber < 0) {
-            return "ERROR";
-    } else if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number') {
-        return "ERROR"
-    } else if  (Number.isInteger(i)) {
-      sum += i;
-        }}
-        return sum;
-    };
-
-
+    let sum = 0;
+    for (let i = minNumber; i <= maxNumber; i++) {
+        if (Number.isInteger(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+};
 // Do not edit below this line
 module.exports = sumAll;
