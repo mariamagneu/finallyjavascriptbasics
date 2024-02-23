40136e7086fc0b908d98b4ea8dcd28d83a9c49ef
maria magdalena <neumariamagdalena@gmail.com>
2024-02-23 20:28:11 +0100

    make for loop recognize if arguments are non numbers - return error, negative - return error,  and make sure the first number is smaller than the last and exchange if needed

--- pages/js/jsexercise.js
+++ pages/js/jsexercise.js
@@ -1,12 +1,18 @@
-
 const sumAll = function (firstNumber, lastNumber) { 
     let sum = 0;
-    for (i = firstNumber; i <= lastNumber; i++) {
-        if (Number.isInteger(i)) {
+    const minNumber = Math.min(firstNumber, lastNumber);
+    const maxNumber = Math.max(firstNumber, lastNumber);
+    for (i = minNumber; i <= maxNumber; i++) {
+        if (minNumber < 0 || maxNumber < 0) {
+            return "ERROR";
+    } else if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number') {
+        return "ERROR"
+    } else if  (Number.isInteger(i)) {
       sum += i;
         }}
         return sum;
     };
+
 
 // Do not edit below this line
 module.exports = sumAll;
