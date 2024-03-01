62f41330e576ea24c493e998acede11afa47ec60
maria magdalena <neumariamagdalena@gmail.com>
2024-03-01 21:01:51 +0100

    create function that listens to click events, stores input in a new variable, and cleans the variable right after

--- pages/js/dynamic.js
+++ pages/js/dynamic.js
@@ -1,3 +1,12 @@
 const list = document.querySelector("ul");
 const newItem = document.querySelector("input");
 const actionButtons = document.querySelector("button");
+
+function clickButton() {
+    actionButtons.addEventListener("click", function() {
+        let inputValue = newItem.value;
+        inputValue = "";
+    });
+}
+
+clickButton();
