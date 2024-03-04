931d63dd2b537dda535c234f618e1fbf95ef527e
maria magdalena <neumariamagdalena@gmail.com>
2024-03-04 20:45:00 +0100

    move all rock paper scissors related files into the new directory

---
+++ pages/rockpaperscissors/rock.js
@@ -0,0 +1,116 @@
+/* 
+get computer choice (random) - create a function
+get user choice (prompt) make small case
+write function that plays single round (parameters playerSelection & Computer Selection)
+return messages like you won you lost its a tie
+test function with console.log(playRound(playerSelection, computerSelection));
+write new function playGame and nest playRound inside of it. user OR statements or Loop. 
+console.log results of each round and the winner at the end.*/ 
+
+let choices = ["rock", "paper", "scissors"];
+let randomIndex;
+const winningCombos = {
+    'rock': 'scissors',
+    'paper': 'rock',
+    'scissors': 'paper',
+}
+let userWins = 0;
+let computerWins = 0;
+
+function playGame() {
+    function getComputerChoice() {
+        randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
+    }
+    function getUserChoice() {
+        let userChoice = prompt("What do you choose? Rock, Paper, or Scissors?").toLowerCase();
+        while (!choices.includes(userChoice)) {
+            userChoice = prompt("You entered an invalid answer. Please choose between Rock, Paper, and Scissors").toLowerCase();
+        }
+        return userChoice;
+    }
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "It's a tie. Nobody wins"; 
+        } else if (winningCombos[playerSelection] === computerSelection) {
+            return "You won!!!";
+        } else {
+            return "You lost! :(";
+        }
+    }
+
+    console.log("Round 1:");
+    let playerSelection = getUserChoice();
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+
+    console.log("Computer chose: " + computerSelection);
+    console.log("You chose: " + playerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    if (result === "You won!!!") {
+        userWins++;
+    } else if (result === "You lost! :(") {
+        computerWins++;
+    }
+    console.log("Round 2:");
+    playerSelection = getUserChoice();
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    console.log("Computer chose: " + computerSelection);
+    console.log("You chose: " + playerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    if (result === "You won!!!") {
+        userWins++;
+    } else if (result === "You lost! :(") {
+        computerWins++;
+    }
+    console.log("Round 3:");
+    playerSelection = getUserChoice();
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    console.log("Computer chose: " + computerSelection);
+    console.log("You chose: " + playerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    if (result === "You won!!!") {
+        userWins++;
+    } else if (result === "You lost! :(") {
+        computerWins++;
+    }
+    console.log("Round 4:");
+    playerSelection = getUserChoice();
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    console.log("Computer chose: " + computerSelection);
+    console.log("You chose: " + playerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    if (result === "You won!!!") {
+        userWins++;
+    } else if (result === "You lost! :(") {
+        computerWins++;
+    }
+    console.log("Round 5:");
+    playerSelection = getUserChoice();
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection);
+    console.log("Computer chose: " + computerSelection);
+    console.log("You chose: " + playerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    if (result === "You won!!!") {
+        userWins++;
+    } else if (result === "You lost! :(") {
+        computerWins++;
+    }
+}
+
+playGame();
+
+if (userWins === computerWins) {
+    console.log("It's a tie.")
+} else if (userWins > computerWins) {
+    console.log("YOU MADE IT! YOU WON THE WHOLE GAME")
+} else {
+    console.log("Try again. I believe in you!")
+}
+
+console.log (userWins)
+console.log (computerWins)
