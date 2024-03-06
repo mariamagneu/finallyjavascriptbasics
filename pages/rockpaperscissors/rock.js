4fd87298cd24e27b97d2097932bf701ca1283cda
maria magdalena <neumariamagdalena@gmail.com>
2024-03-06 10:34:05 +0100

    change getUserChoice into a variable, since I had to remove the getUserChoice function (because It was prompting the User instead of waiting for him/her to click the buttons)

--- pages/rockpaperscissors/rock.js
+++ pages/rockpaperscissors/rock.js
@@ -1,98 +1,83 @@
-
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("Script executed");
 
+    let choices = ["rock", "paper", "scissors"];
+    let randomIndex;
+    const winningCombos = {
+        'rock': 'scissors',
+        'paper': 'rock',
+        'scissors': 'paper',
+    };
+    let userWins = 0;
+    let computerWins = 0;
 
-    console.log("Script executed")
-let choices = ["rock", "paper", "scissors"];
-let randomIndex;
-const winningCombos = {
-    'rock': 'scissors',
-    'paper': 'rock',
-    'scissors': 'paper',
-}
-let userWins = 0;
-let computerWins = 0;
+    const rockBtn = document.createElement("button");
+    rockBtn.textContent = "Rock";
+    document.body.appendChild(rockBtn);
 
+    const scissorsBtn = document.createElement("button");
+    scissorsBtn.textContent = "Scissors";
+    document.body.appendChild(scissorsBtn);
 
+    const paperBtn = document.createElement("button");
+    paperBtn.textContent = "Paper";
+    document.body.appendChild(paperBtn);
 
-function playGame() {
-    function getComputerChoice() {
-        randomIndex = Math.floor(Math.random() * choices.length);
-        return choices[randomIndex];
-    }
-    function getUserChoice() {
-        let userChoice = prompt("What do you choose? Rock, Paper, or Scissors?").toLowerCase();
-        while (!choices.includes(userChoice)) {
-            userChoice = prompt("You entered an invalid answer. Please choose between Rock, Paper, and Scissors").toLowerCase();
+    let getUserChoice = "";
+
+    rockBtn.addEventListener("click", function() {
+        getUserChoice = "rock";
+        playGame();
+    });
+
+    scissorsBtn.addEventListener("click", function() {
+        getUserChoice = "scissors";
+        playGame();
+    });
+
+    paperBtn.addEventListener("click", function() {
+        getUserChoice = "paper";
+        playGame();
+    });
+
+    function playGame() {
+        function getComputerChoice() {
+            randomIndex = Math.floor(Math.random() * choices.length);
+            return choices[randomIndex];
         }
-        return userChoice;
-    }
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            return "It's a tie. Nobody wins"; 
-        } else if (winningCombos[playerSelection] === computerSelection) {
-            return "You won!!!";
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection === computerSelection) {
+                return "It's a tie. Nobody wins"; 
+            } else if (winningCombos[playerSelection] === computerSelection) {
+                return "You won!!!";
+            } else {
+                return "You lost! :(";
+            }
+        }
+
+        console.log("Round 1:");
+        let playerSelection = getUserChoice;
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+
+        console.log("Computer chose: " + computerSelection);
+        console.log("You chose: " + playerSelection);
+        console.log(result);
+        if (result === "You won!!!") {
+            userWins++;
+        } else if (result === "You lost! :(") {
+            computerWins++;
+        }
+       
+        if (userWins === computerWins) {
+            console.log("It's a tie.");
+        } else if (userWins > computerWins) {
+            console.log("YOU MADE IT! YOU WON THE WHOLE GAME");
         } else {
-            return "You lost! :(";
+            console.log("Try again. I believe in you!");
         }
+
+        console.log("User Wins: " + userWins);
+        console.log("Computer Wins: " + computerWins);
     }
-
-    console.log("Round 1:");
-    let playerSelection = getUserChoice();
-    let computerSelection = getComputerChoice();
-    let result = playRound(playerSelection, computerSelection);
-
-    console.log("Computer chose: " + computerSelection);
-    console.log("You chose: " + playerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    if (result === "You won!!!") {
-        userWins++;
-    } else if (result === "You lost! :(") {
-        computerWins++;
-    }
-   
-}
-
-playGame();
-
-if (userWins === computerWins) {
-    console.log("It's a tie.")
-} else if (userWins > computerWins) {
-    console.log("YOU MADE IT! YOU WON THE WHOLE GAME")
-} else {
-    console.log("Try again. I believe in you!")
-}
-
-console.log (userWins)
-console.log (computerWins)
-
-const rockBtn = document.createElement("button");
-rockBtn.textContent = "Rock";
-document.body.appendChild(rockBtn);
-
-const scissorsBtn = document.createElement("button");
-scissorsBtn.textContent = "Scissors";
-document.body.appendChild(scissorsBtn);
-
-const paperBtn = document.createElement("button");
-document.body.appendChild(paperBtn);
-paperBtn.textContent = "Paper";
-
-
-
-rockBtn.addEventListener("click", function() {
-    playerSelection = "rock";
-    playRound();
-});
-
-scissorsBtn.addEventListener("click", function() {
-    playerSelection = "scissors";
-    playRound();
-});
-
-paperBtn.addEventListener("click", function() {
-    playerSelection = "paper";
-    playRound();
-});
-
 });
